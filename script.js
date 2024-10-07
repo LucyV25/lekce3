d5762a4d32e41f5fb760d183a5741640fa9c05d7
Lucie Volemanová <lucievoleman@gmail.com>
2024-10-07 18:57:09 +0200

    mzda

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const jmeno = prompt("Zadej jmeno")
+/*const jmeno = prompt("Zadej jmeno")
 const vek = Number(prompt("zadej věk"))
 
 
 
 document.body.innerHTML += "<p>" + jmeno + ", věk: " + vek+ "</p>"
+*/
+
+const hodinovka = Number(prompt("hodinovka v Kč"))
+const pocetHodin = Number(prompt("pocet hodin"))
+const pocetDni = Number(prompt("počet dní"))
+const hrubaMzda = Number(hodinovka*pocetHodin*pocetDni)
+document.body.innerHTML += "<p>" + "Hrubá mzda je: " + hrubaMzda+ "</p>"
